216aa641f8c1ddad095bcccdc28ff47ecff1bc08
Jon <jonathan.stott@gmail.com>
2025-04-19 14:07:44 +0100

    Wrap in pathname

--- lib/sinatra/simple_assets/handlebars.rb
+++ lib/sinatra/simple_assets/handlebars.rb
@@ -40,7 +40,7 @@ module Sinatra
 
         def path
           require 'handlebars/source'
-          @path ||= ::Handlebars::Source.bundled_path
+          @path ||= Pathname(::Handlebars::Source.bundled_path)
         end
       end
     end
